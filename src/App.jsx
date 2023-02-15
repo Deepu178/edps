4dad1fecb54e5f7cf743f153cd26b698e36f328b
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-02-16 01:11:39 +0530

    remove useless comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { NavBar } from "./components/NavBar";
-// import { Banner } from "./components/Banner";
-// import { Skills } from "./components/Skills";
-// import { Projects } from "./components/Projects";
-// import { Guests } from './components/Guests';
-// import { Contact } from "./components/Contact";
-// import { Footer } from "./components/Footer";
+
 import {Homepage} from './components/Homepage';
 import Dashboard from './components/Dashboard/Dashboard'
 // import styles from './index.css';
